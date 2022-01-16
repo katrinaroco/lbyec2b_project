7e83862a006ec01ccdaae314259867ace29cb4f8
katrinaroco <92778177+katrinaroco@users.noreply.github.com>
2022-01-16 22:11:52 +0800

    Add files via upload

---
+++ project_main.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>  
+#include <stdlib.h>
+#include <string.h> 
+#include "project.h"
+
+int main()
+{
+	int city, answer;
+	
+	start:
+	printf("Metro Manila Hospital Locator & Registration\n");
+	printf("Please enter your city: \n");
+	printf("1. Manila\n2. Makati\n3. Quezon\n");
+	scanf("%d", &city);
+	
+	switch(city){
+		case 1:
+			printf("Hospitals around Manila\n");
+			printf("..presenting choices\n");
+			answer = askregistration();
+			createinfo();
+			break;
+		case 2:
+			printf("Hospitals around Makati\n");
+			printf("..presenting choices\n");
+			answer = askregistration();
+			createinfo();
+			break;
+		case 3:
+			printf("Hospitals around Quezon City\n");
+			printf("..presenting choices\n");
+			answer = askregistration();
+			createinfo();			
+			break;
+	}
+
+	printf("yay!\n");
+
+
+}
